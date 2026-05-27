64e0d748439678c4df5b964527e765bc46195af7
Adnan Jukker <93384175+AdnanJukker@users.noreply.github.com>
2026-05-27 22:39:51 +0530

    Update 16. 3Sum Closest.cpp

--- 16. 3Sum Closest.cpp
+++ 16. 3Sum Closest.cpp
@@ -25,7 +25,7 @@ public:
             {
                 return res;
             }
-            if(target < total)
+            if(target > total)
             {
                 left++;
             }
