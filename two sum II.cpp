54112949369e09ea33a1e6cf1fd3dc45f509085c
Adnan Jukker <93384175+AdnanJukker@users.noreply.github.com>
2026-05-28 21:20:15 +0530

    Update two sum II.cpp

--- two sum II.cpp
+++ two sum II.cpp
@@ -1,43 +1,25 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        int i = 0;
-        int j = numbers.size() - 1;
+        int i = 0 ;
+            int j = numbers.size() - 1;
 
-        while (i < j) {
+        while(i<j)
+        {
             int sum = numbers[i] + numbers[j];
 
-            if (sum == target) {
+            if (sum == target)
+            {
                 return {i + 1, j + 1};
             }
 
-            if (sum < target) {
+            if(sum<target)
+            {
                 i++;
-            } else {
+            } else{
                 j--;
             }
         }
-
         return {};
     }
 };
-
-int main() {
-    Solution obj;
-
-    vector<int> numbers = {2, 7, 11, 15};
-    int target = 9;
-
-    vector<int> result = obj.twoSum(numbers, target);
-
-    cout << "Indices: ";
-    for (int x : result) {
-        cout << x << " ";
-    }
-
-    return 0;
-}
